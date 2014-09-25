5a2acf52c7aabbd1fbc6e05dc55a53ac64979dc5
Andreas Linz <klingt.net@gmail.com>
2014-09-25 16:59:13 +0200

    implement module pattern

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,31 @@
 'use strict';
 
-window.onresize = resizeChatDiv;
+var darkStar = {};
 
-function resizeChatDiv() {
-    var chatDiv = document.getElementById('chat-div');
-    var videoDiv = document.getElementById('video-div');
-    var msgDiv = document.getElementById('message-div');
-    var msgsDiv = document.getElementById('messages-div');
+darkStar.main = function(){
+    window.onresize = resizeChatDiv;
 
-    if(videoDiv && chatDiv && msgDiv && msgsDiv) {
-        var chatHeight = window.innerHeight - videoDiv.clientHeight;
-        if(chatHeight > 0) {
-            console.log(chatHeight);
-            chatDiv.style.height = chatHeight+"px";
-            var msgDivHeight = msgDiv.clientHeight;
-            var msgsDivHeight = (chatHeight - msgDivHeight) * 0.75;
-            msgsDiv.style.height = Math.floor(msgsDivHeight)+"px";
+    function resizeChatDiv() {
+        var chatDiv = document.getElementById('chat-div');
+        var videoDiv = document.getElementById('video-div');
+        var msgDiv = document.getElementById('message-div');
+        var msgsDiv = document.getElementById('messages-div');
+
+        if(videoDiv && chatDiv && msgDiv && msgsDiv) {
+            var chatHeight = window.innerHeight - videoDiv.clientHeight;
+            if(chatHeight > 0) {
+                console.log(chatHeight);
+                chatDiv.style.height = chatHeight+"px";
+                var msgDivHeight = msgDiv.clientHeight;
+                var msgsDivHeight = (chatHeight - msgDivHeight) * 0.70;
+                msgsDiv.style.height = Math.floor(msgsDivHeight)+"px";
+            }
         }
     }
-}
 
-resizeChatDiv();
+    return {
+        'resizeChatDiv': resizeChatDiv
+    }
+}();
+
+darkStar.main.resizeChatDiv();
